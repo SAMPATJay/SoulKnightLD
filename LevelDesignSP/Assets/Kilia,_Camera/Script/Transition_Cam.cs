dc284f5a449f1de1de3b1a2b6c72dad1b09dd50e
Kilian-YES <allairekilian@gmail.com>
2019-02-11 18:38:00 +0100

    Finish-Kilian

--- LevelDesignSP/Assets/Kilia,_Camera/Script/Transition_Cam.cs
+++ LevelDesignSP/Assets/Kilia,_Camera/Script/Transition_Cam.cs
@@ -27,7 +27,7 @@ public class Transition_Cam : MonoBehaviour
 
             foreach (GameObject go in cameraArray)
             {
-                Debug.Log("Hello");
+
                 go.SetActive(false);
                 cameraArray[0].SetActive(true);
             }
@@ -38,9 +38,119 @@ public class Transition_Cam : MonoBehaviour
 
             foreach (GameObject go in cameraArray)
             {
-                Debug.Log("Hello45154");
+
                 go.SetActive(false);
                 cameraArray[1].SetActive(true);
+            }
+        }
+
+        if (other.gameObject.tag == "trig3")
+        {
+
+            foreach (GameObject go in cameraArray)
+            {
+
+                go.SetActive(false);
+                cameraArray[2].SetActive(true);
+            }
+        }
+
+        if (other.gameObject.tag == "trig4")
+        {
+
+            foreach (GameObject go in cameraArray)
+            {
+
+                go.SetActive(false);
+                cameraArray[3].SetActive(true);
+            }
+        }
+
+        if (other.gameObject.tag == "trig5")
+        {
+
+            foreach (GameObject go in cameraArray)
+            {
+
+                go.SetActive(false);
+                cameraArray[4].SetActive(true);
+            }
+        }
+
+        if (other.gameObject.tag == "trig6")
+        {
+
+            foreach (GameObject go in cameraArray)
+            {
+
+                go.SetActive(false);
+                cameraArray[5].SetActive(true);
+            }
+        }
+
+        if (other.gameObject.tag == "trig7")
+        {
+
+            foreach (GameObject go in cameraArray)
+            {
+
+                go.SetActive(false);
+                cameraArray[6].SetActive(true);
+            }
+        }
+
+        if (other.gameObject.tag == "trig8")
+        {
+
+            foreach (GameObject go in cameraArray)
+            {
+
+                go.SetActive(false);
+                cameraArray[7].SetActive(true);
+            }
+        }
+
+        if (other.gameObject.tag == "trig9")
+        {
+
+            foreach (GameObject go in cameraArray)
+            {
+
+                go.SetActive(false);
+                cameraArray[8].SetActive(true);
+            }
+        }
+
+        if (other.gameObject.tag == "trig10")
+        {
+
+            foreach (GameObject go in cameraArray)
+            {
+
+                go.SetActive(false);
+                cameraArray[9].SetActive(true);
+            }
+        }
+
+        if (other.gameObject.tag == "trig11")
+        {
+
+            foreach (GameObject go in cameraArray)
+            {
+
+                go.SetActive(false);
+                cameraArray[10].SetActive(true);
+            }
+        }
+
+        if (other.gameObject.tag == "trig12")
+        {
+
+            foreach (GameObject go in cameraArray)
+            {
+
+                go.SetActive(false);
+                cameraArray[11].SetActive(true);
             }
         }
     }
