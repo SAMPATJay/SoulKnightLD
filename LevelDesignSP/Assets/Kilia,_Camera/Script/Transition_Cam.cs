98f5b60127c6f05ad31ea46c45ac7ba28861ebd0
Kilian-YES <allairekilian@gmail.com>
2019-02-11 17:49:33 +0100

    boi

---
+++ LevelDesignSP/Assets/Kilia,_Camera/Script/Transition_Cam.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transition_Cam : MonoBehaviour
+{
+
+    [SerializeField]  GameObject[] cameraArray;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.gameObject.tag == "trig1")
+        {
+
+            foreach (GameObject go in cameraArray)
+            {
+                Debug.Log("Hello");
+                go.SetActive(false);
+                cameraArray[0].SetActive(true);
+            }
+        }
+
+        if (other.gameObject.tag == "trig2")
+        {
+
+            foreach (GameObject go in cameraArray)
+            {
+                Debug.Log("Hello45154");
+                go.SetActive(false);
+                cameraArray[1].SetActive(true);
+            }
+        }
+    }
+}
